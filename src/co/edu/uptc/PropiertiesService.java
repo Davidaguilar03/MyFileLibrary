a94ddda7f28b6a81e900ce72bcf70fd5253417d5
David <aguilarcastillo89@gmail.com>
2024-09-04 13:06:19 -0500

    first commit

---
+++ src/co/edu/uptc/PropiertiesService.java
@@ -0,0 +1,29 @@
+package co.edu.uptc;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.util.Properties;
+
+public class PropiertiesService {
+   private String filePath;
+
+    public String getValue(String Key) {
+        Properties properties = new Properties();
+        String value = "";
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            properties.load(fileInputStream);
+            value=properties.getProperty(Key);
+        } catch (IOException e) {
+            System.out.println("Error al leer propiedades: " + e.getMessage());
+        }
+        return value;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }   
+
+
+
+ 
+}
